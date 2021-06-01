5a81c8b4881c9b6dea6d7c656914e810034576d8
Rodri <rodrigo-1905@hotmail.com>
2021-06-01 20:32:27 -0300

    hago cambios para conectarme a la base de datos

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+
+    "nombreBBDD"=>"d4n8a9gnv5nu3a",
+    "nombreServidor"=>"ec2-107-21-10-179.compute-1.amazonaws.com",
+    "nombreUsuario"=>"mdecpvhomnvpcw",
+    "pass"=>"fae5d587e4492123278706bb60585994a6def39e287769bc980a3bd25ff18e14"
+
+);
